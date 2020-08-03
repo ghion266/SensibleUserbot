86e608f7f95e7af74b9572ffad0e892464e1eb60
ghion266 <64766760+ghion266@users.noreply.github.com>
2020-08-03 13:15:21 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -10,19 +10,17 @@ from userbot.utils import admin_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 ALIVE_IMG = "https://telegra.ph/file/6225d447ed8ba1d23079f.jpg"
-ALIVE_caption = "`Sensible Userbot IS:` **ONLINE**\n\n"
+ALIVE_caption = "`GTS Userbot IS:` **ONLINE**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**Sensible  OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `Sensible Userbot Sat-2.95`\n\n"
-ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
+ALIVE_caption += "**GTS  OS** : `3.14`\n\n
+ALIVE_caption += "**Current Sat** : `GTS Userbot Sat-2.95`\n\n"
+ALIVE_caption += f"**GTS** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**Bot Made By @ceowhitehatcracks \n\n"
-ALIVE_caption += "Copyright By [CEOWHITEHATCRACKS](GitHub.com/spandey112)\n\n"
-ALIVE_caption += "[Deploy SensibleUserbot](GitHub.com/spandey112/SensibleUserbot)"
-#@command(outgoing=True, pattern="^.alive$")
+ALIVE_caption += "**Bot Made By @Gts12 \n\n"
+#@commandoutgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
